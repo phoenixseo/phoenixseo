940656e21439834547146c30956287e028c22594
Frank Pfabigan <frank.pfabigan@gmail.com>
2019-10-20 14:28:46 +0200

    drupal 8.7.8

---
+++ README.md
@@ -0,0 +1,11 @@
+# phoenixseo webapp
+
+Webapp for [phoenixseo.de](https://phoenixseo.de).
+
+## Tech Base
+
+Drupal-8, composer-based.
+
+## Creator
+
+This project is compiled and built by [PHOENIXSEO.de Frank Pfabigan e.K.](https://phoenixseo.de).
---
+++ web/modules/custom/mt_countdown/js/mt_countdown.js
@@ -0,0 +1,28 @@
+/*
+ * MTCountDown scripts
+ *  Add any module related script here
+ */
+(function ($, Drupal, drupalSettings) {
+
+  Drupal.behaviors.mt_countdown = {
+    attach: function (context, settings) {
+
+      // Add html element with class "mt-count-down" after the body.
+      $(document).ready(function() {
+        $('.mt-count-down-inject').prependTo('body');
+      });
+
+      // Get access setting from 'drupalSettings'.
+      var expiration_date = drupalSettings.mt_countdown.expiration_date;
+
+      // Start countdown script.
+      $(".mt-count-down-slot").countdown(expiration_date, function(event) {
+        $(this).html(event.strftime('' +
+          '<span class="mt-time"><span class="mt-count">%D</span> <span class="mt-label">day%!d</span></span> ' +
+          '<span class="mt-time"><span class="mt-count">%H</span> <span class="mt-label">hours</span></span> ' +
+          '<span class="mt-time"><span class="mt-count">%M</span> <span class="mt-label">minutes</span></span> ' +
+          '<span class="mt-time"><span class="mt-count">%S</span> <span class="mt-label">seconds</span></span>'));
+      });
+    }
+  };
+})(jQuery, Drupal, drupalSettings);
